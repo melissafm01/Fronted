38675b13796b09c3126ba43298a809d227290271
Melissaf M <melissam200604@gmail.com>
2025-07-05 20:47:50 -0500

    axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,22 +4,9 @@ import { API_URL } from "../config";
 const instance = axios.create({
   baseURL: `${API_URL}/api`,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
+
 });
 
-// Interceptor para manejar erroress
-instance.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response && error.response.status === 401) {
-      // Manejar logout si el token es inválido
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
+
 
 export default instance;
